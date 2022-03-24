dd9a3690cc6cf86385a70021496b52b5072d4de4
김동현 <rlaehdgus3543@naver.com>
2022-03-24 15:32:59 +0900

    22/03/24 update

--- src/App copy.vue
+++ src/App copy.vue
@@ -29,66 +29,27 @@ export default {
    
     const todos = ref([]);
     const error = ref('');
-    
-    const getTodos = async () => {
-      try{
-        const res = await axios.get('http://localhost:3000/todos');
-        todos.value = res.data;
-      } catch(err){
-        console.log(err);
-        error.value = "something is wrong";
-      }
-    };
 
-    getTodos();
-
-    const addTodo = async (todo) => {
+    const addTodo = (todo) => {
       error.value=''
-      try{
-        const res = await axios.post('http://localhost:3000/todos',{
+      axios.post('http://localhost:3000/todos',{
         subject: todo.subject,
         completed: todo.completed,
-        });
-        todos.value.push(res.data);
-      }catch(err){
+      }).then(res => {
+        console.log(res);
+        todos.value.push(todo);
+      }).catch(err => {
         console.log(err);
         error.value = "something is wrong";
-      }
-      
-      // .then(res => {
-      //   console.log(res);
-      //   todos.value.push(todo);
-      // }).catch(err => {
-      //   console.log(err);
-      //   error.value = "something is wrong";
-      // });
+      });
     }   
 
-    const deleteTodo = async (index) => {
-      error.value= '';
-      const id = todos.value[index].id;
-      try{
-        await axios.delete('http://localhost:3000/todos/'+id);
-        todos.value.splice(index, 1);
-      }catch(err){
-        console.log(err);
-        error.value = "something is wrong";
-      }
-      
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
     }
 
-    const toggleTodo = async (index) => {
-      const id = todos.value[index].id;
-      try{
-        await axios.patch('http://localhost:3000/todos/'+id,{
-          completed: !todos.value[index].completed
-        });
-        todos.value[index].completed = !todos.value[index].completed;
-      }catch(err){
-        console.log(err);
-        error.value = "something is wrong";
-      }
-      
+    const toggleTodo = (index) => {
+      todos.value[index].completed = !todos.value[index].completed;
     }
 
     const searchText = ref('');
